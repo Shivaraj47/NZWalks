dd2e3d84a33b9241599afc304f688d175c0a4c11
Shivaraj <shivarajhavaldar14@gmail.com>
2023-12-11 13:48:39 +0530

    End of Section 2

---
+++ NZWalks/NZWalks.API/Data/NZWalkDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Model.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalkDbContext: DbContext 
+    {
+        public NZWalkDbContext(DbContextOptions<NZWalkDbContext> options): base(options)
+        {
+            
+        }
+
+        //create Dbset properties
+
+        public DbSet<Region> regions { get; set; }
+        public DbSet<Walk> walks { get; set; }
+
+        public DbSet<WalkDifficulty> walkDifficulties { get; set; }
+    }
+}
---
+++ NZWalks/NZWalks.API/Model/Domain/Walk.cs
@@ -0,0 +1,19 @@
+﻿namespace NZWalks.API.Model.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public double Length { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+
+        //Navigation Property
+
+        public Region region { get; set; }
+        public WalkDifficulty walkDifficulty { get; set; }
+
+
+    }
+}
