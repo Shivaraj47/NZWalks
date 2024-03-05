7f99d17c60f358654d168fe898c06563ebda55c1
Shivaraj <shivarajhavaldar14@gmail.com>
2024-03-05 20:41:51 +0530

    New commit

--- NZWalks/NZWalks.API/Controllers/AuthController.cs
+++ NZWalks/NZWalks.API/Controllers/AuthController.cs
@@ -23,6 +23,7 @@ namespace NZWalks.API.Controllers
         public async Task<IActionResult> LoginAsync(LoginRequestDto loginRequestDto)
         {
             //Check if user authenticated or not
+            //make it for git
             var user = await userRepository.AuthenticateAsync
                 (loginRequestDto.Username, loginRequestDto.Password);
 
