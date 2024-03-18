5ba320c2b43843935c796e7af2b986ca06b10cdd
Shivaraj <shivarajhavaldar14@gmail.com>
2024-03-18 22:34:43 +0530

    FDAFFLT:102: Added editor config file

--- NZWalks/NZWalks.API/Controllers/WalksController.cs
+++ NZWalks/NZWalks.API/Controllers/WalksController.cs
@@ -1,6 +1,5 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NZWalks.API.Model.Domain;
 using NZWalks.API.Model.DTO;
@@ -8,111 +7,135 @@ using NZWalks.API.Repository;
 
 namespace NZWalks.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class WalksController : ControllerBase
-    {
-        private readonly IRegionRepository regionRepository;
-        private readonly IWalkRepository walkRepository;
-        private readonly IMapper mapper;
-
-        
-        public WalksController(IWalkRepository walkRepository, IMapper mapper)
-        {
-            this.walkRepository = walkRepository;
-            this.mapper = mapper;
-        }
-        [HttpGet]
-        [Authorize(Roles = "reader")]
-        public async Task<IActionResult> GetALlAsync()
-        {
-            //Getting data from database
-           var walkdomain = await walkRepository.GetAllWalksAsync();
-
-            //convert Domain to DTO
-           var walkDto = mapper.Map<List<WalkDto>>(walkdomain);
-
-            return Ok(walkDto);
-
-            
-        }
-
-        [HttpGet]
-        [Route("{id:Guid}")]
-        [Authorize(Roles = "reader")]
-        public async Task<IActionResult> GetWalkByIdAsync(Guid id)
-
-        {
-            //Getting data from domain
-            var walkDomain = await walkRepository.GetWalkByIdAsync(id);
-
-            //convert Domain to DTO
-            var walkDto = mapper.Map<WalkDto>(walkDomain);
-            return Ok(walkDto);
-        }
-
-        [HttpPost]
-        [Authorize(Roles = "writer")]
-        public async Task<IActionResult> CreateWalkAsync([FromBody] AddWalkRequestDTO addWalkRequestDTO)
-        {
-            //Convert DTO to Domainmodel
-            var walkDomain = mapper.Map<Walk>(addWalkRequestDTO);
+	[Route("api/[controller]")]
+	[ApiController]
+	public class WalksController : ControllerBase
+	{
+		private readonly IRegionRepository regionRepository;
+		private readonly IWalkRepository walkRepository;
+		private readonly IMapper mapper;
 
 
-            // Pass details to repository
+		public WalksController(IWalkRepository walkRepository, IMapper mapper)
+		{
+			this.walkRepository = walkRepository;
+			this.mapper = mapper;
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		[HttpGet]
+		[Authorize(Roles = "reader")]
+		public async Task<IActionResult> GetALlAsync()
+		{
+			//Getting data from database
+			var walkdomain = await walkRepository.GetAllWalksAsync();
 
-            walkDomain = await walkRepository.CreateWalkAsync(walkDomain);
+			//convert Domain to DTO
+			var walkDto = mapper.Map<List<WalkDto>>(walkdomain);
 
-            //Convert Domain model to DTO 
-            //Making sure you use DTO
+			return Ok(walkDto);
 
-            var walkDto = mapper.Map<WalkDto>(walkDomain);
-            
-            return Ok(walkDto);
-        }
 
-        [HttpPut]
-        [Route("{id:Guid}")]
-        [Authorize(Roles = "writer")]
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
 
-        public async Task<IActionResult> UpdateWalkAsync([FromRoute]Guid id,[FromBody] UpdateWalkRequestDto updateWalkRequest)
-        {
-            //convert DTO to Domain Model
+		[HttpGet]
+		[Route("{id:Guid}")]
+		[Authorize(Roles = "reader")]
+		public async Task<IActionResult> GetWalkByIdAsync(Guid id)
 
-            var walkdomain = mapper.Map<Walk>(updateWalkRequest);
+		{
+			//Getting data from domain
+			var walkDomain = await walkRepository.GetWalkByIdAsync(id);
 
-            //Update Walk using repository
+			//convert Domain to DTO
+			var walkDto = mapper.Map<WalkDto>(walkDomain);
+			return Ok(walkDto);
+		}
 
-            walkdomain = await walkRepository.UpdateWalkAsync(id,walkdomain);
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="addWalkRequestDTO"></param>
+		/// <returns></returns>
+		[HttpPost]
+		[Authorize(Roles = "writer")]
+		public async Task<IActionResult> CreateWalkAsync([FromBody] AddWalkRequestDTO addWalkRequestDTO)
+		{
+			//Convert DTO to Domainmodel
+			var walkDomain = mapper.Map<Walk>(addWalkRequestDTO);
 
-            if(walkdomain == null)
-            {
-                return NotFound(id);
-            }
 
-            //Convert Domain to DTO
-            var walkDto = mapper.Map<WalkDto>(walkdomain);
-            return Ok(walkDto);
-        }
+			// Pass details to repository
 
-        [HttpDelete]
-        [Route("{id:Guid}")]
-        [Authorize(Roles = "writer")]
+			walkDomain = await walkRepository.CreateWalkAsync(walkDomain);
 
-        public async Task<IActionResult> DeleteWalkAsync([FromRoute] Guid id)
-        {
-            var walksDomain = await walkRepository.DeleteWalkAsync(id);
+			//Convert Domain model to DTO 
+			//Making sure you use DTO
 
-            //check the id is exist or not
-            if(walkRepository == null)
-            {
-                return NotFound();
-            }
+			var walkDto = mapper.Map<WalkDto>(walkDomain);
 
-            //Convert Domain mode to DTO
+			return Ok(walkDto);
+		}
 
-            var walkDto = mapper.Map<WalkDto> (walksDomain);
-            return Ok(walkDto);
-        }
-    }
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="id"></param>
+		/// <param name="updateWalkRequest"></param>
+		/// <returns></returns>
+		[HttpPut]
+		[Route("{id:Guid}")]
+		[Authorize(Roles = "writer")]
+
+		public async Task<IActionResult> UpdateWalkAsync([FromRoute] Guid id, [FromBody] UpdateWalkRequestDto updateWalkRequest)
+		{
+			//convert DTO to Domain Model
+
+			var walkdomain = mapper.Map<Walk>(updateWalkRequest);
+
+			//Update Walk using repository
+
+			walkdomain = await walkRepository.UpdateWalkAsync(id, walkdomain);
+
+			if (walkdomain == null)
+			{
+				return NotFound(id);
+			}
+
+			//Convert Domain to DTO
+			var walkDto = mapper.Map<WalkDto>(walkdomain);
+			return Ok(walkDto);
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
+		[HttpDelete]
+		[Route("{id:Guid}")]
+		[Authorize(Roles = "writer")]
+
+		public async Task<IActionResult> DeleteWalkAsync([FromRoute] Guid id)
+		{
+			var walksDomain = await walkRepository.DeleteWalkAsync(id);
+
+			//check the id is exist or not
+			if (walkRepository == null)
+			{
+				return NotFound();
+			}
+
+			//Convert Domain mode to DTO
+
+			var walkDto = mapper.Map<WalkDto>(walksDomain);
+			return Ok(walkDto);
+		}
+	}
 }
