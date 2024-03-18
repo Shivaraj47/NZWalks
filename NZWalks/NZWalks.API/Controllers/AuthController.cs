a85d6c75cbbaaee5775eb1d17cfa44a7bd831d14
Shivaraj <shivarajhavaldar14@gmail.com>
2024-03-18 19:29:42 +0530

    Revert "New commit i just added"
    This reverts commit b5237a2aafe49cfca3dee2d04cd208582bb220e9.

--- NZWalks/NZWalks.API/Controllers/AuthController.cs
+++ NZWalks/NZWalks.API/Controllers/AuthController.cs
@@ -17,11 +17,6 @@ namespace NZWalks.API.Controllers
             this.userRepository = userRepository;
             this.tokenHandler = tokenHandler;
         }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="loginRequestDto"></param>
-        /// <returns></returns>
 
         [HttpPost]
         [Route("login")]
