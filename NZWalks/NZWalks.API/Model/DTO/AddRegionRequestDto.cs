cd2b4f30ed847b11be64aa7cf9c2eef0f4232aeb
Shivaraj47 <58481362+Shivaraj47@users.noreply.github.com>
2024-03-19 12:13:42 +0530

    FDAFFLT-402: Added summary and Description tags (#12)
    * FDAFFLT-401: Added editor config file
    
    * FDAFFLT-401: Comment removed
    
    * FDAFFLT-402: Added summary and Description tag

--- NZWalks/NZWalks.API/Model/DTO/AddRegionRequestDto.cs
+++ NZWalks/NZWalks.API/Model/DTO/AddRegionRequestDto.cs
@@ -1,13 +1,31 @@
 ﻿namespace NZWalks.API.Model.DTO
 {
-    public class AddRegionRequestDto
-    {
-        public string Code { get; set; }
-        public string Name { get; set; }
-        public double Area { get; set; }
-        public double Lat { get; set; }
-        public double Long { get; set; }
+	public class AddRegionRequestDto
+	{
+		public string Code
+		{
+			get; set;
+		}
+		public string Name
+		{
+			get; set;
+		}
+		public double Area
+		{
+			get; set;
+		}
+		public double Lat
+		{
+			get; set;
+		}
+		public double Long
+		{
+			get; set;
+		}
 
-        public long Population { get; set; }
-    }
+		public long Population
+		{
+			get; set;
+		}
+	}
 }
