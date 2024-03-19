6ee79c4c60b173e0302f5a3a0ab970227dd3c239
Shivaraj <shivarajhavaldar14@gmail.com>
2024-03-19 08:32:11 +0530

    FDAFFLT-401: Added editor config file

--- NZWalks/NZWalks.API/Model/DTO/AddWalkRequestDTO.cs
+++ NZWalks/NZWalks.API/Model/DTO/AddWalkRequestDTO.cs
@@ -1,12 +1,24 @@
 ﻿namespace NZWalks.API.Model.DTO
 {
-    public class AddWalkRequestDTO
-    {
-        public string Name { get; set; }
+	public class AddWalkRequestDTO
+	{
+		public string Name
+		{
+			get; set;
+		}
 
-        public double Length { get; set; }
-        public Guid RegionId { get; set; }
-        public Guid WalkDifficultyId { get; set; }
+		public double Length
+		{
+			get; set;
+		}
+		public Guid RegionId
+		{
+			get; set;
+		}
+		public Guid WalkDifficultyId
+		{
+			get; set;
+		}
 
-    }
+	}
 }
