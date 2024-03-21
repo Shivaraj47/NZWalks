d7e21e60efceb9c6a402300a6131cdd5af21859f
Shivaraj47 <58481362+Shivaraj47@users.noreply.github.com>
2024-03-21 14:54:20 +0530

    FDAFFLT-11: Added editor config file (#18)

--- NZWalks/NZWalks.API/Repository/TokenHandler.cs
+++ NZWalks/NZWalks.API/Repository/TokenHandler.cs
@@ -6,46 +6,48 @@ using System.Text;
 
 namespace NZWalks.API.Repository
 {
-    public class TokenHandler : ITokenHandler
-    {
-        private readonly IConfiguration configuration;
+	public class TokenHandler : ITokenHandler
+	{
+		private readonly IConfiguration configuration;
 
-        public TokenHandler(IConfiguration configuration)
-        {
-            this.configuration = configuration;
-        }
-        public Task<string> CreateTokenAsync(User user)
-        {
-            
-
-            //Create claims
-            var claims = new List<Claim>();
-            claims.Add(new Claim(ClaimTypes.GivenName, user.FirtName));
-            claims.Add(new Claim(ClaimTypes.Surname, user.LastName));
-            claims.Add(new Claim(ClaimTypes.Email, user.EmailAddress));
-
-            //loop into roles of users
-            user.Roles.ForEach((role) =>
-            {
-                claims.Add(new Claim(ClaimTypes.Role, role));
-            });
-
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
-            var credentals = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
-
-            var token = new JwtSecurityToken(
-                configuration["Jwt:Issuer"],
-                configuration["Jwt:Audience"],
-                claims,
-                expires: DateTime.Now.AddMinutes(15),
-                signingCredentials: credentals);
-
-            return Task.FromResult(new JwtSecurityTokenHandler().WriteToken(token));
-
-                
+		public TokenHandler(IConfiguration configuration)
+		{
+			this.configuration = configuration;
+		}
+		public Task<string> CreateTokenAsync(User user)
+		{
 
 
+			//Create claims
+			var claims = new List<Claim>
+			{
+				new Claim(ClaimTypes.GivenName, user.FirtName),
+				new Claim(ClaimTypes.Surname, user.LastName),
+				new Claim(ClaimTypes.Email, user.EmailAddress)
+			};
 
-        }
-    }
+			//loop into roles of users
+			user.Roles.ForEach((role) =>
+			{
+				claims.Add(new Claim(ClaimTypes.Role, role));
+			});
+
+			var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
+			var credentals = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+			var token = new JwtSecurityToken(
+				configuration["Jwt:Issuer"],
+				configuration["Jwt:Audience"],
+				claims,
+				expires: DateTime.Now.AddMinutes(15),
+				signingCredentials: credentals);
+
+			return Task.FromResult(new JwtSecurityTokenHandler().WriteToken(token));
+
+
+
+
+
+		}
+	}
 }
