9b16f3e61cdbedca9978866c569fb4faea66b403
Shivaraj47 <58481362+Shivaraj47@users.noreply.github.com>
2024-03-21 19:27:20 +0530

    Changes adding to Main branch (#24)
    * FDAFFLT-21:Deleted Home Controller (#21)
    
    * FDAFFLT-23: Added summary tag (#23)
    
    * FDAFFLT-22: Added editor config file (#22)

--- NZWalks/NZWalks.API/Profiles/WalksProfile.cs
+++ NZWalks/NZWalks.API/Profiles/WalksProfile.cs
@@ -4,18 +4,18 @@ using NZWalks.API.Model.DTO;
 
 namespace NZWalks.API.Profiles
 {
-    public class WalksProfile: Profile
-    {
-        public WalksProfile()
-        {
-            CreateMap<Walk,WalkDto>().ReverseMap();
-            CreateMap<WalkDifficulty,WalksDifficultyDto>().ReverseMap();
-            CreateMap<AddWalkRequestDTO,Walk>().ReverseMap();
-            CreateMap<UpdateWalkRequestDto,Walk>().ReverseMap();
-            CreateMap<WalksDifficultyDto, WalkDifficulty>().ReverseMap();
-            CreateMap<AddDifficultyDTO,WalkDifficulty>().ReverseMap();
-            CreateMap<UpdateDifficultyDto,WalkDifficulty>().ReverseMap();
-        }
+	public class WalksProfile : Profile
+	{
+		public WalksProfile()
+		{
+			CreateMap<Walk, WalkDto>().ReverseMap();
+			CreateMap<WalkDifficulty, WalksDifficultyDto>().ReverseMap();
+			CreateMap<AddWalkRequestDTO, Walk>().ReverseMap();
+			CreateMap<UpdateWalkRequestDto, Walk>().ReverseMap();
+			CreateMap<WalksDifficultyDto, WalkDifficulty>().ReverseMap();
+			CreateMap<AddDifficultyDTO, WalkDifficulty>().ReverseMap();
+			CreateMap<UpdateDifficultyDto, WalkDifficulty>().ReverseMap();
+		}
 
-    }
+	}
 }
--- NZWalks/NZWalks.API/Program.cs
+++ NZWalks/NZWalks.API/Program.cs
@@ -15,27 +15,27 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
-    var securityScheme = new OpenApiSecurityScheme
-    {
-        Name = "JWT Authentication",
-        Description = "Enter a valid JWT bearer token",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-        Scheme = "bearer",
-        BearerFormat = "JWT",
-        Reference = new OpenApiReference
-        {
-            Id = JwtBearerDefaults.AuthenticationScheme,
-            Type = ReferenceType.SecurityScheme
-        }
+	var securityScheme = new OpenApiSecurityScheme
+	{
+		Name = "JWT Authentication",
+		Description = "Enter a valid JWT bearer token",
+		In = ParameterLocation.Header,
+		Type = SecuritySchemeType.Http,
+		Scheme = "bearer",
+		BearerFormat = "JWT",
+		Reference = new OpenApiReference
+		{
+			Id = JwtBearerDefaults.AuthenticationScheme,
+			Type = ReferenceType.SecurityScheme
+		}
 
-    };
+	};
 
-    options.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {securityScheme,new string[] {} }
-    });
+	options.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+	options.AddSecurityRequirement(new OpenApiSecurityRequirement
+	{
+		{securityScheme,new string[] {} }
+	});
 
 });
 
@@ -43,26 +43,26 @@ builder.Services.AddDbContext<NZWalkDbContext>(Option =>
 Option.UseSqlServer(builder.Configuration.GetConnectionString("NZWalks")));
 
 builder.Services.AddScoped<IRegionRepository, SQLRegionRepository>();
-builder.Services.AddScoped<IWalkRepository,SQLWalkRepository>();
-builder.Services.AddScoped<IDifficultyRepository,SQLDifiicultyRepository>();
+builder.Services.AddScoped<IWalkRepository, SQLWalkRepository>();
+builder.Services.AddScoped<IDifficultyRepository, SQLDifiicultyRepository>();
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<ITokenHandler,NZWalks.API.Repository.TokenHandler>();
+builder.Services.AddScoped<ITokenHandler, NZWalks.API.Repository.TokenHandler>();
 
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(option =>
-    option.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-    });
+	.AddJwtBearer(option =>
+	option.TokenValidationParameters = new TokenValidationParameters
+	{
+		ValidateIssuer = true,
+		ValidateAudience = true,
+		ValidateLifetime = true,
+		ValidateIssuerSigningKey = true,
+		ValidIssuer = builder.Configuration["Jwt:Issuer"],
+		ValidAudience = builder.Configuration["Jwt:Audience"],
+		IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+	});
 
 
 
@@ -71,8 +71,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
